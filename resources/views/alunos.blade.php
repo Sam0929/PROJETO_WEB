4e6582ea8ec711c809bd55cab0316c1daa728bad
Samuel VaninI <samuel.v@puccampinas.edu.br>
2022-10-10 13:15:08 -0300

    sql inicial

---
+++ resources/views/alunos.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('title','Alunos Matriculados')
+
+@section('content')
+
+
+
+<p>Alunos serão adicionados aqui. Com seu id: {{ $id }}</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
