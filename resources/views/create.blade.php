8688f06e46cffa1cbc5a43214798c9fd45ba06f3
SAMUEL VANINI <samuel.v@puccampinas.edu.br>
2022-10-11 10:49:40 -0300

    nova interface

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -6,17 +6,29 @@
 
     <form action= "{{ route('registrar') }}" method="POST">
         @csrf
-        <label for="">Nome</label> <br />
-        <input type="text" name="nome"> <br />
-        <label for="">Matéria</label> <br />
-        <input type="text" name="matéria"> <br />
-        <label for="">Professor</label> <br />
-        <input type="text" name="professor"> <br />
-        <label for="">Filmes</label> <br />
-        <input type="text" name="filmes"> <br />
-        <label for="">Descrição</label> <br />
-        <input type="text" name="descrição"> <br />
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Nome</label>
+            <input type="text" name="Nome" class="form-control" id="exampleFormControlInput1" placeholder="Nome..">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Matéria</label>
+            <input type="text" name="Matéria" class="form-control" id="exampleFormControlInput1" placeholder="Matéria..">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Professor</label>
+            <input type="text" name="Professor" class="form-control" id="exampleFormControlInput1" placeholder="Professor..">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Filme</label>
+            <input type="text" name="Filme" class="form-control" id="exampleFormControlInput1" placeholder="Filme..">
+        </div>
+        <div class="mb-7">
+            <label for="exampleFormControlTextarea1" class="form-label">Descrição</label>
+            <input type="text" name="Descrição" class="form-control" id="exampleFormControlInput1" placeholder="Descrição..">
+            
+        </div>
             <button>Salvar</button>
     </form>
+    
 
 @endsection
