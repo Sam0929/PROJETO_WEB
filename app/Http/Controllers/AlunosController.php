ac21708b6a4aec0fedaee2bf52ca08c665b9a1ec
Guilherme Lopes Silva <guilopez2004@gmail.com>
2022-10-17 16:16:45 -0300

    api no Controller

--- app/Http/Controllers/AlunosController.php
+++ app/Http/Controllers/AlunosController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Aluno;
+use Illuminate\Support\Facades\Http;
+use Nette\Utils\Json;
 use Redirect;
 class AlunosController extends Controller
 
@@ -20,8 +22,30 @@ class AlunosController extends Controller
     }
 
     public function new (){
+        $filmes = array();
 
-        return view ('CRUD.create');
+        $auxcategories =  Http::get('https://www.learn-laravel.cf/categories');
+        $categories = json_decode($auxcategories, true);
+
+        for($j=1; $j<7; $j++) {
+
+            $api = Http::get('https://www.learn-laravel.cf/movies?page=' . $j);
+            $auxjson = json_decode($api, true);
+            $api = $auxjson['data'];
+
+            foreach ($api as $filme){
+                for($i=0 ; $i<6; $i++){
+                    if($filme['category_id'] == $i+1){
+                        $filmes[] = array(
+                            'id' => $filme['id'], 
+                            'nome' => $filme['name'],
+                            'category' => $categories[$i]['name']);
+                    }
+                }
+            };
+        }
+        
+        return view ('CRUD.create', ['filmes' => $filmes]);
     }
 
     public function add (Request $request){
@@ -33,7 +57,30 @@ class AlunosController extends Controller
     }
     public function edit ($id) {
         $Aluno = Aluno::findOrFail($id);
-        return view('CRUD.create', ['Aluno' => $Aluno]);
+        $filmes = array();
+
+        $auxcategories =  Http::get('https://www.learn-laravel.cf/categories');
+        $categories = json_decode($auxcategories, true);
+
+        for($j=1; $j<7; $j++) {
+
+            $api = Http::get('https://www.learn-laravel.cf/movies?page=' . $j);
+            $auxjson = json_decode($api, true);
+            $api = $auxjson['data'];
+
+            foreach ($api as $filme){
+                for($i=0 ; $i<6; $i++){
+                    if($filme['category_id'] == $i+1){
+                        $filmes[] = array(
+                            'id' => $filme['id'], 
+                            'nome' => $filme['name'],
+                            'category' => $categories[$i]['name']);
+                    }
+                }
+            };
+        }
+        
+        return view('CRUD.create', ['Aluno' => $Aluno, 'filmes' => $filmes]);
     }
     
     public function update($id, Request $request){
